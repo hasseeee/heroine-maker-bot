602c230b6be98e2a7facb5f26845051acf95bf85
takuya hasegawa <hasegawa.takuya1204@gmail.com>
2025-05-31 00:27:20 +0900

    feat: perserをhandlerに変更

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, Request, HTTPException
-from linebot import LineBotApi, WebhookParser
+from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
 import os
@@ -13,7 +13,7 @@ CHANNEL_ACCESS_TOKEN = os.getenv("CHANNEL_ACCESS_TOKEN")
 CHANNEL_SECRET = os.getenv("CHANNEL_SECRET")
 
 line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
-parser = WebhookParser(CHANNEL_SECRET)
+handler = WebhookHandler(CHANNEL_SECRET)
 
 
 @app.post("/webhook")
@@ -22,22 +22,18 @@ async def callback(request: Request):
     signature = request.headers.get("X-Line-Signature", "")
 
     try:
-        events = parser.parse(body.decode("utf-8"), signature)
+        handler.handle(body.decode("utf-8"), signature)
     except InvalidSignatureError:
         raise HTTPException(status_code=400, detail="Invalid signature")
-
-    for event in events:
-        if isinstance(event, MessageEvent) and isinstance(event.message, TextMessage):
-            user_message = event.message.text.strip()
-            
-            if user_message == "おはよう":
-                line_bot_api.reply_message(
-                    event.reply_token,
-                    TextSendMessage(text="おはよう")
-            )
-
+ 
     return {"status": "ok"}
 
+@handler.add(MessageEvent)
+def handle_message(event):
+    text = event.message.text.strip()
+    if text == "おはよう":
+        reply = TextSendMessage(text="おはよう")
+        line_bot_api.reply_message(event.reply_token, reply)
 
 #仮想環境に入ってね：Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope Process
 # .\venv\Scripts\activate
