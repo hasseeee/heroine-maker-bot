2537bc05141f1648cdd41e65388614289a1c758a
takuya hasegawa <hasegawa.takuya1204@gmail.com>
2025-05-31 00:49:54 +0900

    feat: 大輝コピペ

--- main.py
+++ main.py
@@ -1,44 +1,54 @@
-from fastapi import FastAPI, Request, HTTPException
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, TextSendMessage
-import os
+from linebot.models import MessageEvent, TextMessage
+from fastapi import FastAPI, Request, BackgroundTasks, Header
 from dotenv import load_dotenv
+from starlette.exceptions import HTTPException
+import os
 
 load_dotenv()
 
+LINE_BOT_API=LineBotApi(os.environ["CHANNEL_ACCESS_TOKEN"])
+handler=WebhookHandler(os.environ["CHANNEL_SECRET"])
 app = FastAPI()
 
-CHANNEL_ACCESS_TOKEN = os.getenv("CHANNEL_ACCESS_TOKEN")
-CHANNEL_SECRET = os.getenv("CHANNEL_SECRET")
 
-line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
-handler = WebhookHandler(CHANNEL_SECRET)
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
 
 
-@app.post("/webhook")
-async def callback(request: Request):
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str = None):
+    return {"item_id": item_id, "q": q}
+
+@app.post("/callback")
+async def callback(
+    request: Request,
+    background_tasks: BackgroundTasks,
+    x_line_signature=Header(None),
+):
     body = await request.body()
-    signature = request.headers.get("X-Line-Signature", "")
 
     try:
-        handler.handle(body.decode("utf-8"), signature)
+        background_tasks.add_task(
+            handler.handle, body.decode("utf-8"), x_line_signature
+        )
     except InvalidSignatureError:
         raise HTTPException(status_code=400, detail="Invalid signature")
- 
-    return {"status": "ok"}
+
+    return "ok"
 
 @handler.add(MessageEvent)
 def handle_message(event):
-    message_text = event.message.text.strip()
-    if "おはよう" in message_text:
-        reply = TextSendMessage(text="おはよう")
-        line_bot_api.reply_message(event.reply_token, reply)
-
-#仮想環境に入ってね：Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope Process
-# .\venv\Scripts\activate
-# uvicorn main:app --reload
-# uvicorn main:app --host   
-#pip install fastapi uvicorn python-dotenv requests
-# pip install line-bot-sdk
-#uvicorn main:app --reload --port 8000
+    message_text = event.message.text.lower()
+    
+    if "こんにちは" in message_text:
+        message = TextMessage(text="こんにちは！！")
+        LINE_BOT_API.reply_message(event.reply_token, message)
+    elif "ありがとう" in message_text:
+        message = TextMessage(text="こちらこそー")
+        LINE_BOT_API.reply_message(event.reply_token, message)
+    else :
+        message = TextMessage(text="いつも使ってくれてありがとう")
+        LINE_BOT_API.reply_message(event.reply_token, message)
